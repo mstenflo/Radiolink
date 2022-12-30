3a57a27eb9b4bda9dcc44a94cd1563ddcfcca412
Martin Stenflo <stenflo@gmail.com>
2022-12-30 11:36:37 -0700

    touch test

--- public/script.js
+++ public/script.js
@@ -34,8 +34,8 @@ document.querySelector('#dronezone').addEventListener('touchstart', buttonPress)
 
 function buttonPress(e) {
   // e.preventDefault();
-  console.log(e);
+  // console.log(e);
   // console.log(e.path[0].id);
-  document.getElementById("kgnu").innerHTML = "touched"
-  client.publish('stenflo/radiolink', e.path[0].id.toString());
+  document.getElementById(e.target.id).innerHTML = "touched"
+  client.publish('stenflo/radiolink', e.target.id.toString());
 }
